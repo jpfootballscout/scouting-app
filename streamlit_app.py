23ce62e5d523355bd31af09eabf81f0feaee789c
jpfootballscout <jp.footballscout@gmail.com>
2026-05-30 17:25:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+from datetime import datetime
 
+# Google Sheets auth
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
+# Open sheet
+sheet = client.open("scouting_reports").sheet1
+
+# UI
 st.title("⚽ Football Scouting Platform")
 
 st.write("Witaj w systemie scoutingowym.")
@@ -8,10 +28,20 @@ zawodnik = st.text_input("Imię i nazwisko zawodnika")
 
 pozycja = st.selectbox(
     "Pozycja",
-    ["BR", "LO", "PO", "ŚO", "6", "8", "10", "Skrzydło", "9"]
+    ["BR", "ŚO", "LO", "PO", "DP", "ŚP", "10", "LS", "PS", "N"]
 )
 
 ocena = st.slider("Ocena zawodnika", 1, 10)
 
 if st.button("Zapisz raport"):
+
+    data = datetime.now().strftime("%Y-%m-%d")
+
+    sheet.append_row([
+        data,
+        zawodnik,
+        pozycja,
+        ocena
+    ])
+
     st.success("Raport został zapisany!")
